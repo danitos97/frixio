dcd254a689582011593dcd6758c39d5380c811f4
Daniel Rivera <daniel_doni@hotmail.com>
2023-07-13 21:20:26 -0600

    phpmailer

--- server/email.php
+++ server/email.php
@@ -1,15 +1,41 @@
 <?php 
-    session_start();
-    
-    if(!isset($_SESSION["frixio-user"])) exit;
 
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "no_contestar@frixio.com.mx";
-    $to = "daniel_doni@hotmail.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+session_start();
+
+if(!isset($_SESSION["frixio-user"])) exit;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try{
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = getenv("SMTP_USER");           //SMTP username
+    $mail->Password   = getenv("SMTP_PASS");                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;      
+    
+    $mail->setFrom(getenv("SMTP_USER"), 'FRIXIO');    //Add a recipient
+    $mail->addAddress('daniel_doni@hotmail.com');               //Name is optional
+
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
 ?> 
